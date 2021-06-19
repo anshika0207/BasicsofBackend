9b5d7ed6b488221235e29be78dfa07af85560657
Anshika Bhatt <anshikabhatt0207@gmail.com>
2021-06-19 11:43:42 +0530

    Add functions to todo

--- ToDo_List/app.js
+++ ToDo_List/app.js
@@ -5,10 +5,15 @@ const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("Public"));
 
-mongoose.connect('mongodb://localhost:27017/todoDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/todoDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const listSchema = new mongoose.Schema({
   name: {
@@ -17,7 +22,13 @@ const listSchema = new mongoose.Schema({
   }
 });
 
+const customListSchema = new mongoose.Schema({
+  name: String,
+  items: [listSchema]
+})
+
 const Item = mongoose.model("Item", listSchema);
+const Custom = mongoose.model("Custom", customListSchema);
 
 const Food = new Item({
   name: "Eat food"
@@ -31,47 +42,43 @@ const Happy = new Item({
   name: "Be happy!"
 })
 
+const defaultArray = [Food, Sleep, Happy]
 
+app.get("/", function(req, res) {
 
-
-// Item.deleteMany({}, function(err){
-//   if(err){
-//     console.log(err);
-//   }
-//   else{
-//     console.log("Deleted all successfully!");
-//   }
-// })
-
-app.get("/", function(req, res){
-
-  Item.find({},function(err, items){
-    if(items.length==0){
-      Item.insertMany([Food, Sleep, Happy], function(err){
-        if(err){
+  Item.find({}, function(err, items) {
+    if (items.length == 0) {
+      Item.insertMany(defaultArray, function(err) {
+        if (err) {
           console.log(err);
-        }
-        else{
+        } else {
           console.log("All inserted successfully");
         }
       })
-       res.redirect("/");
-    }
-    else{
-      res.render("index", {listTitle: "Today", newListItems: items});
+      res.redirect("/");
+    } else {
+      res.render("index", {
+        listTitle: "Today",
+        newListItems: items
+      });
     }
   })
 
 });
 
-app.get("/:other", function(req,res){
+app.get("/:other", function(req, res) {
   var name = req.params.other;
-  res.render("other", {
-    other: name
+
+  const custom = new Custom({
+    name: name,
+    items: defaultArray
   })
+
+  custom.save();
+
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
   // console.log(req.body);
   var inp = req.body.addedItem;
@@ -80,11 +87,10 @@ app.post("/", function(req, res){
     name: inp
   });
 
-  Item.insertMany(added, function(err, added){
-    if(err){
+  Item.insertMany(added, function(err, added) {
+    if (err) {
       console.log(err);
-    }
-    else{
+    } else {
       console.log("New item added");
     }
   });
@@ -101,13 +107,14 @@ app.post("/", function(req, res){
 //   res.send()
 // });
 
-app.post("/delete", function(req,res){
+app.post("/delete", function(req, res) {
   var checkedId = req.body.checkbox;
-  Item.deleteOne({_id: checkedId}, function(err){
-    if(err){
+  Item.deleteOne({
+    _id: checkedId
+  }, function(err) {
+    if (err) {
       console.log(err);
-    }
-    else{
+    } else {
       console.log("Item deleted");
     }
   });
@@ -115,6 +122,6 @@ app.post("/delete", function(req,res){
 })
 
 
-app.listen(3000, function(req, res){
+app.listen(3000, function(req, res) {
   console.log("server up and running on port 3000");
 })
